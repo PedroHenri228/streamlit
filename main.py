c1ef81ff5e2d7ddeb7626708b743dc90ce255317
Pedro Henrique de Oliveira Monteiro <165577690+PedroHenri228@users.noreply.github.com>
2024-08-18 18:20:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import streamlit as st
 import pandas as pd
 
-
+# Configurações da página
 st.set_page_config(page_title='Testando o Streamlit')
 
+# Cabeçalho e título
 with st.container():
-    st.subheader("MEu primeiro site com o Streamlit")
+    st.subheader("Meu primeiro site com o Streamlit")
     st.title("Dashboard")
-    
-@st.cache_data
 
+# Função para carregar os dados
+@st.cache_data
 def carregar_dados():
     tabela = pd.read_csv("layout/resultados.csv")
-    
     return tabela
 
-
+# Exibição do gráfico
 with st.container():
+    st.write("---")
     dados = carregar_dados()
-    
     st.area_chart(dados, x='Nome', y='Idade')
